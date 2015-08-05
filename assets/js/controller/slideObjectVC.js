ab692793a325c99ed645361e58f0aed57b866718
Fan Zhang <tonyjohnvan@gmail.com>
2015-08-05 17:18:54 -0400

    added changes on list item change

--- assets/js/controller/slideObjectVC.js
+++ assets/js/controller/slideObjectVC.js
@@ -84,3 +84,80 @@ function findSlideItem(id){
     }
     return null;
 }
+
+
+var SOModiStr = '\
+<div class="container">\
+<div class="row black-nav">\
+    <ul class="nav navbar-nav col-xm-4">\
+        <li class="">\
+            <a href="#" onclick="{document.execCommand(\'bold\', false);}" class="dropdown-toggle" data-toggle="dropdown"\
+            aria-expanded="false">\
+                <i class="glyphicon glyphicon-bold"></i>\
+            </a>\
+        </li>\
+        <li class="">\
+            <a href="#" onclick="{document.execCommand(\'italic\', false);}" class="dropdown-toggle" data-toggle="dropdown"\
+            aria-expanded="false">\
+                <i class="glyphicon glyphicon-italic"></i>\
+            </a>\
+        </li>\
+        <li class="">\
+            <a href="#" onclick="{document.execCommand(\'justifyLeft\', false);}" class="dropdown-toggle" data-toggle="dropdown"\
+            aria-expanded="false">\
+                <i class="glyphicon glyphicon-align-left"></i>\
+            </a>\
+        </li>\
+    </ul>\
+    <ul class="nav navbar-nav col-xm-4">\
+    <li class="">\
+        <a href="#" onclick="{document.execCommand(\'justifyCenter\', false);}" class="dropdown-toggle" data-toggle="dropdown"\
+        aria-expanded="false">\
+            <i class="glyphicon glyphicon-align-center"></i>\
+        </a>\
+    </li>\
+    <li class="">\
+    <a href="#" onclick="{document.execCommand(\'justifyRight\', false);}" class="dropdown-toggle" data-toggle="dropdown"\
+    aria-expanded="false">\
+        <i class="glyphicon glyphicon-align-right"></i>\
+    </a>\
+    </li>\
+<li class="">\
+    <a href="#" onclick="{document.execCommand(\'insertOrderedList\', false);}" class="dropdown-toggle" data-toggle="dropdown"\
+    aria-expanded="false">\
+        <i class="glyphicon glyphicon-th-list"></i>\
+    </a>\
+</li>\
+</ul>\
+<ul class="nav navbar-nav col-xm-4">\
+    <li class="">\
+        <a href="#" onclick="{document.execCommand(\'insertUnorderedList\', false);}" class="dropdown-toggle" data-toggle="dropdown"\
+        aria-expanded="false">\
+            <i class="glyphicon glyphicon-list"></i>\
+        </a>\
+    </li>\
+    <li class="">\
+        <div class="dropdown">\
+            <button class="btn btn-default dropdown-toggle" type="button" id="dropdownMenu1" data-toggle="dropdown" aria-haspopup="true" aria-expanded="true">\
+            FontSize\
+                <span class="caret"></span>\
+            </button>\
+            <ul class="dropdown-menu" aria-labelledby="dropdownMenu1">\
+                <li><a href="#" onclick="{document.execCommand(\'fontSize\', false, 1);}">XXSmall</a></li>\
+                <li><a href="#" onclick="{document.execCommand(\'fontSize\', false, 2);}">XSmall</a></li>\
+                <li><a href="#" onclick="{document.execCommand(\'fontSize\', false, 3);}">Small</a></li>\
+                <li><a href="#" onclick="{document.execCommand(\'fontSize\', false, 4);}">Normal</a></li>\
+                <li><a href="#" onclick="{document.execCommand(\'fontSize\', false, 5);}">Large</a></li>\
+                <li><a href="#" onclick="{document.execCommand(\'fontSize\', false, 6);}">XLarge</a></li>\
+                <li><a href="#" onclick="{document.execCommand(\'fontSize\', false, 7);}">XXLarge</a></li>\
+            </ul>\
+        </div>\
+    </li>\
+</ul>\
+</div>\
+</div>\
+    <div class="container inputWrap">\
+        <div class="row">\
+            <div id="directText" contenteditable="true"></div>\
+        </div>\
+    </div>';
